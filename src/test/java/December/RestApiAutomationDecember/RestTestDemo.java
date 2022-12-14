78b5b785a7e1146242559f62af4f140d733715ce
Narinderpal Singh <narinders163@gmail.com>
2022-12-14 13:16:59 -0500

    This is a Commit From Demo

---
+++ src/test/java/December/RestApiAutomationDecember/RestTestDemo.java
@@ -0,0 +1,10 @@
+package December.RestApiAutomationDecember;
+
+public class RestTestDemo {
+
+	public static void main(String[] args) {
+		System.out.println("This is being tested for GIT");
+
+	}
+
+}
